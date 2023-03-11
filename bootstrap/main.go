1a8d792c0d3f1c3969c6bf7b54093d950bf5557f
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-03-11 23:14:54 +0800

    Code auto format

--- bootstrap/main.go
+++ bootstrap/main.go
@@ -1,20 +1,20 @@
 package bootstrap
 
 import (
-	"gonga/app/Http/Middlewares"
+	middlewares "gonga/app/Http/Middlewares"
 	"gonga/database"
 	"gonga/packages"
 	"gonga/routes"
 	"net/http"
 
-	"gorm.io/gorm"
 	"github.com/pterm/pterm"
+	"gorm.io/gorm"
 )
 
 // Application represents the Golang application instance.
 type Application struct {
 	Router *packages.MyRouter
-	DB *gorm.DB
+	DB     *gorm.DB
 }
 
 // NewApplication creates a new instance of the Golang application.
@@ -31,17 +31,19 @@ func (app *Application) RegisterApiRoutes() {
 	app.Router.Use(middlewares.CorsMiddleware)
 	app.Router.Use(middlewares.ThrottleMiddleware)
 
-	routes.RegisterApiRoutes(app.Router,app.DB)
+	routes.RegisterApiRoutes(app.Router, app.DB)
 }
+
 // ConnectDatabase connects to database.
 func (app *Application) ConnectDatabase() error {
-    var err error
-    app.DB, err = database.Connect()
-    if err != nil {
-        return err
-    }
-    return nil
+	var err error
+	app.DB, err = database.Connect()
+	if err != nil {
+		return err
+	}
+	return nil
 }
+
 // Run starts the Golang application.
 func (app *Application) Run() error {
 	pterm.Info.Println("Server started on [http://localhost:8000]")
