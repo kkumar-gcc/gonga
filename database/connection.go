6592cc4069ce4ce50bc4caa33057470383088f88
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-03-11 23:26:19 +0800

    Make silent connection to GORM

--- database/connection.go
+++ database/connection.go
@@ -13,8 +13,10 @@ func Connect() (*gorm.DB, error) {
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbConfig.User, dbConfig.Password, dbConfig.Host,dbConfig.Port, dbConfig.Database)
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
+
 	if err != nil {
 		panic("Failed to connect to database!")
 	}
