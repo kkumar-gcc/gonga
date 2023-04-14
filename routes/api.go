ba496e2e78038b93463b6dbbbe91664b09915068
kkumar-gcc <krishkumar9352@gmail.com>
2023-04-15 02:33:26 +0530

    Added different endpoints

--- routes/api.go
+++ routes/api.go
@@ -51,13 +51,11 @@ func RegisterApiRoutes(router *packages.MyRouter, db *gorm.DB) {
 	// Search API endpoint handlers
 	router.Get("/search", SearchController.Index)
 
-
-
-
 	// ******************************
 	// *    ALERT: DO NOT EDIT!     *
 	// * This area is non-editable. *
 	// ******************************
+
 	// Register Auth Routes
 	RegisterAuthRoutes(router, db)
 }
